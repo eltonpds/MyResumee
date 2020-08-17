eb6d2e0a548401f4c881b8877204714fc63b0f0a
Elton Pereira <eltonpereira94@hotmail.com>
2020-08-17 19:11:44 -0300

    Removendo declaração desnecessária

--- MyResumee.API/ClientApp/src/app/core/login/login.module.ts
+++ MyResumee.API/ClientApp/src/app/core/login/login.module.ts
@@ -8,7 +8,7 @@ import { LoginService } from './services/login.service';
 
 
 @NgModule({
-  declarations: [ LoginComponent ],
+  declarations: [  ],
   imports: [
     CommonModule,
     LoginRoutingModule,
